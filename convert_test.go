55ad2e216244504264196f13fe8e8da1b2970632
walkmiao <372815340@qq.com>
2021-05-13 09:41:55 +0800

    clear main function

--- convert_test.go
+++ convert_test.go
@@ -1,4 +1,4 @@
-package main
+package mhtml2html
 
 import "testing"
 
--- mhtml2html.go
+++ mhtml2html.go
@@ -1,4 +1,4 @@
-package main
+package mhtml2html
 
 import (
 	"bytes"
@@ -8,11 +8,9 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"mime"
 	"mime/multipart"
 	"net/http"
-	"net/http/httptest"
 	"net/mail"
 	"net/url"
 	"os"
@@ -24,7 +22,6 @@ import (
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/andybalholm/cascadia"
-	"github.com/pkg/browser"
 
 	"github.com/tdewolff/minify/v2"
 	"github.com/tdewolff/minify/v2/css"
@@ -359,156 +356,4 @@ func ConvertMht2HTML(mht string) (string, error) {
 	}
 	return buf.String(), nil
 
-}
-
-func main() {
-	log.SetFlags(log.Lshortfile)
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "mhtl2html [opitons] MHTMLFILE \r\n Options: \r\n")
-		flag.PrintDefaults()
-	}
-	flag.Var(&removingElementArray, "re", "repeatablely optional: jquery like elements selector to be removed")
-	flag.Var(&removingAttrArray, "ra", "repeatablely optional: pairs of jquery like elements selector and attribute to be removed")
-	flag.Parse()
-	if (flag.NArg() != 1) || (len(removingAttrArray)%2 != 0) {
-		flag.Usage()
-		os.Exit(0)
-	}
-
-	f, err := os.Open(flag.Arg(0))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	msg, err := mail.ReadMessage(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, params, err := mime.ParseMediaType(msg.Header.Get("Content-Type"))
-	boundary := params["boundary"]
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var minifier *minify.M
-	if *needMinify {
-		minifier = minify.New()
-		minifier.AddFunc("text/css", css.Minify)
-		minifier.AddFunc("text/html", html.Minify)
-		minifier.AddFunc("image/svg+xml", svg.Minify)
-		minifier.AddFuncRegexp(regexp.MustCompile("^(application|text)/(x-)?(java|ecma)script$"), js.Minify)
-		minifier.AddFuncRegexp(regexp.MustCompile("[/+]json$"), json.Minify)
-		minifier.AddFuncRegexp(regexp.MustCompile("[/+]xml$"), xml.Minify)
-	}
-
-	mpr := multipart.NewReader(msg.Body, boundary)
-	initialLoc := ""
-	for {
-		part, err := mpr.NextPart()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		contentLocation := part.Header.Get("Content-Location")
-		base, err := url.Parse(contentLocation)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		data, err := ioutil.ReadAll(part)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if part.Header.Get("Content-Transfer-Encoding") == "base64" {
-			n := base64.StdEncoding.DecodedLen(len(data))
-			buf := make([]byte, n)
-			n, err := base64.StdEncoding.Decode(buf, data)
-			if err != nil {
-				log.Fatal(err)
-			}
-			data = buf[:n]
-		}
-
-		if cid := part.Header.Get("Content-ID"); cid != "" {
-			cid = strings.Trim(cid, "<>")
-			cid2loc[cid] = contentLocation
-		}
-
-		// NOTE: no space and double quotation permitted in contenType of CSS url base64 encoding data
-		contentType := part.Header.Get("Content-Type")
-		contentType = strings.Replace(contentType, "\"", "", -1)
-		contentType = strings.Replace(contentType, "'", "", -1)
-		contentType = strings.Replace(contentType, " ", "", -1)
-		initial := false
-		if initialLoc == "" && (contentType == "text/html" || strings.HasPrefix(contentType, "text/html;")) {
-			initialLoc = contentLocation
-			initial = true
-		}
-
-		if *needMinify {
-			out := buffer.NewWriter(make([]byte, 0, len(data)))
-			if err := minifier.Minify(strings.Split(contentType, ";")[0], out, buffer.NewReader(data)); err == nil {
-				data = out.Bytes()
-			} else {
-				//fmt.Fprintf(os.Stderr, "DEBUG: failed to miniy %s %s. Reason: %s \n", contentType, contentLocation, err)
-			}
-		}
-
-		files[contentLocation] = &file{contentType, base, data, initial, false}
-	}
-
-	if initialLoc == "" {
-		log.Fatal("no HTML pages to display")
-	}
-
-	for _, f := range files {
-		if ct := f.contentType; ct == "text/css" {
-			f.data = modifyCSS(f.base, f.data)
-		}
-	}
-	for _, f := range files {
-		if ct := f.contentType; ct == "text/html" || strings.HasPrefix(ct, "text/html;") {
-			encoding, name, certain := charset.DetermineEncoding(f.data, f.contentType)
-			if name != "utf-8" && !(name == "windows-1252" && !certain) {
-				decoded, err := encoding.NewDecoder().Bytes(f.data)
-				if err != nil {
-					log.Fatal(err)
-				}
-				f.data = decoded
-				if strings.Contains(f.contentType, ";") {
-					f.contentType = strings.SplitN(f.contentType, ";", 2)[0]
-				}
-				f.converted = true
-			}
-			var err error
-			f.data, err = modifyHTML(f.base, f.data, f.converted)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
-
-	if *browsing {
-		srv := httptest.NewServer(http.HandlerFunc(handler))
-		initialURL := srv.URL + "/" + url.PathEscape(initialLoc)
-		if err := browser.OpenURL(initialURL); err != nil {
-			fmt.Fprintf(os.Stderr, "Couldn't start browser:%v", err)
-			fmt.Fprintf(os.Stderr, "Open the following URL manually:%v", initialURL)
-		} else {
-			fmt.Fprintf(os.Stderr, "Browsing: %s", initialURL)
-		}
-
-		select {}
-	} else {
-		for _, f := range files {
-			if ct := f.contentType; ct == "text/html" || strings.HasPrefix(ct, "text/html;") {
-				os.Stdout.Write(f.data)
-			}
-		}
-	}
 }
